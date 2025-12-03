facf2a6b4103249e40c9fcd1674a099b56eae314
achintyakhatana881 <achintyakhatana881@gmail.com>
2025-12-03 10:25:39 +0530

    Add files via upload

---
+++ project.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    float a, b;
+    char op;
+
+    printf("Enter first number: ");
+    scanf("%f", &a);
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &op);
+
+    printf("Enter second number: ");
+    scanf("%f", &b);
+
+    switch(op) {
+        case '+': 
+            printf("Result = %.2f", a + b);
+            break;
+
+        case '-': 
+            printf("Result = %.2f", a - b);
+            break;
+
+        case '*': 
+            printf("Result = %.2f", a * b);
+            break;
+
+        case '/':
+            if(b == 0)
+                printf("Error! Division by zero not allowed.");
+            else
+                printf("Result = %.2f", a / b);
+            break;
+
+        default:
+            printf("Invalid Operator");
+    }
+
+    return 0;
+}
